2eb7f541f9a672718683b1cbbe12bcf14a83aa4f
Henrick <henrick.souza@cs.brazcubas.edu.br>
2026-05-17 14:41:00 -0300

    Update and rename tarefas.py to cadastro.py

---
+++ W pasta/file/cadastro.py
@@ -0,0 +1,42 @@
+from dados import cadastro_filmes
+from utils import titulo, linha
+
+
+def catagol(): #catalogo 1 
+    print("FILME | GÊNERO | ANO | DISPONÍVEL")
+    for i, filme in enumerate(cadastro_filmes, start=1): 
+        print(f"{i} - {filme['Nome']} | {filme['Genero']} | {filme['Ano']} | {filme['Disponível']}")
+    #for filme in cadastro_filmes:
+        #print(f"{filme['Nome']} | {filme['Genero']} | {filme['Ano']} | {filme['Disponível']}")
+
+def cadastrar(): #cadastro de filmes 2 
+    nome = str(input("Cadastrar nome do filme: "))
+    print("Gêneros cadastraveis:")
+    print("1 - Drama | 2 - Ação | 3 - Animação | 4 - Suspense | 5 - Policial | 6 - Comedia | 7 - Ficção")
+    genero = str(input("Escolha o gênero (1 a 7): "))
+    if genero == "1":
+        genero = "Drama"
+    elif genero == "2":
+        genero = "Ação"
+    elif genero == "3":
+        genero = "Animação"
+    elif genero == "4":
+        genero = "Suspense"
+    elif genero == "5":
+        genero = "Policial"
+    elif genero == "6":
+        genero = "Comédia"
+    elif genero == "7":
+        genero = "Ficção"
+    ano = str(input("Cadastre o ano do filme (AAAA): "))
+    disponivel = str(input("O filme está disponível? (Sim/Não): "))
+    filmes = {
+        "ID": len(cadastro_filmes) + 1,
+        "Nome": nome,
+        "Genero": genero,
+        "Ano": ano,
+        "Disponível": disponivel
+    }
+    cadastro_filmes.append(filmes)
+    print("Filme cadastrado!!!")
+    
