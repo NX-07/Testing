89ac546def33e853487c25f96991687947d2ba85
Henrick <henrick.souza@cs.brazcubas.edu.br>
2026-05-17 16:17:57 -0300

    Update main.py

--- W pasta/file/main.py
+++ W pasta/file/main.py
@@ -1,6 +1,6 @@
 from dados import cadastro_filmes, fila_reserva, pilha_devolucao, registro_devolucao
 from utils import titulo, linha
-from cadastro import catagol, cadastrar
+from cadastro import catagol, cadastrar, pgenero 
 
 def start():
     titulo("SISTEMA DE LOCAÇÃO")
@@ -11,15 +11,17 @@ def start():
     4 - Pilha de devoluções                              #LIFO: última devolução aparece primeiro
     5 - Registrar aluguel                                #(marca filme como indisponível)
     6 - Registrar devolução                              #marca filme como disponível
-    7 - Procurar por gênero                              #filtra catálogo por gênero\Bonus
+    7 - Procurar por gênero                              #filtra catálogo por gênero
     8 - Sair                                             #Encerrar programa
     ''')
     linha()
     print()
+    
 def option(): #opção do menu
     while True:
-        opcao = int(input("Digite a opção desejada: "))
+        opcao = int(input("\nDigite uma opção (1-8): "))
         if opcao == 1: 
+            print("\n[Catálogo de filmes]\n")
             catagol()
         elif opcao == 2: 
             cadastrar()
@@ -43,25 +45,30 @@ def option(): #opção do menu
             print("Opção invalida!!")
 
 def fila(): #FILA DE RESERVA 3
-    mostrar = int(input("Ver fila: 1 - reservar filme: 2: "))
+    print('\nFila de Reserva = 1 \nReserva de Filme = 2\n')
+    mostrar = int(input("Digite a opção desejada: "))
     if mostrar == 2:
-        print("Cadastrar reserva!!!")
-        nomafilme = input("Nome do cliente: ")
-        reserva = input("Filme a ser reservado: ")
-        Id_filme = str(input("ID do filme a ser reservado: "))
+        print("\n[Cadastrar reserva!!!]")
+        nomafilme = input("\nNome do cliente: ")
+        reserva = input("\nFilme a ser reservado: ")
+        catagol()
+        Id_filme = str(input("\nID do filme a ser reservado: "))
         fila = {
             "Nomefilme": nomafilme, 
             "Reserva": reserva,
             "Id_filme": Id_filme
         }
         fila_reserva.append(fila)
-        print("fila de reserva cadastrado na fila!!!")
+        print("\nCadastrado na fila de espera realizado!!!")
+        print("Voltando para o menu...")
     else:
         print("Fila de reserva:")
         for fila in fila_reserva:
             print(f"{fila['Nomefilme']} | {fila['Reserva']} | {fila['Id_filme']}")
+        print("Voltando para o menu...")
+            
 def pilha(): #Não finalizajicdsvfsdfbdfbdfb 4
-    print("Pilhas de devolução")
+    print("\n[Pilhas de devolução]\n")
     if len(pilha_devolucao) == 0: #lê a pilha de devolução, se tiver vazia, ele dispara o print
         print("Sem registros de devolução")
     else:
@@ -69,15 +76,15 @@ def pilha(): #Não finalizajicdsvfsdfbdfbdfb 4
             print(f"{pilha_devolu['Nome']} | {pilha_devolu['Genero']} | {pilha_devolu['Ano']} | {pilha_devolu['Disponível']}") #Pega da pilha de devolução, o nome, genero, ano e disponibilidade do filme devolvido e mostra para o usuário
 
 def registro_aluguel(): # Aluguel 5
-    print("Registrar aluguel")
+    print("\n[Registrar aluguel de filme]\n")
     catagol() # mostra o catalogo cadastrado
     if len(cadastro_filmes) == 0: #Não tendo nada cadastrado, ele dispara o print
-        print("Não há filmes cadastrados para alugar.")
+        print("Não há filmes cadastrados para alugar")
         return
     try:
-        id_filme = int(input("Digite o número do filme que deseja alugar: ")) #Cria uma nova variavel
+        id_filme = int(input("\nDigite o ID do filme que deseja alugar: ")) #Cria uma nova variavel
     except ValueError:
-        print("Entrada inválida. Por favor, digite um número válido.")
+        print("Entrada inválida. Digite um número válido")
         return
     for filmes in cadastro_filmes: #filme é variavel local, filme procura dentro do cadastro
         if filmes['ID'] == id_filme: # Se id for igual ao id que o filme achou ele entra na condição
@@ -94,15 +101,15 @@ def registro_aluguel(): # Aluguel 5
             else:
                 print("Opção invalida")
 def registro_dev(): #devolução 6    
-    print("Registrar devolução")
+    print("\nRegistrar devolução\n")
     catagol() # Catalogo cadastrado
     if len(cadastro_filmes) == 0: #procura dentro do cadastro, se tem algum dentro
-        print("Não há filmes cadastrados para devolver.")
+        print("Não há filmes cadastrados para devolver")
         return
     try:
-        id_filme = int(input("Digite o número do filme que deseja devolver: ")) #Id do filme que não é mostrado, mas é sinalizado como 'i' no codigo
+        id_filme = int(input("\nDigite o ID do filme que deseja devolver: ")) #Id do filme que não é mostrado, mas é sinalizado como 'i' no codigo
     except ValueError:
-        print("Entrada inválida. Por favor, digite um número válido.")
+        print("Entrada inválida. Por favor, digite um número válido")
         return
     for filmes in cadastro_filmes: # filme é variavel local, filme procura dentro do cadastro
         if filmes['ID'] == id_filme: # Se id for igual ao id que o filme achou ele entra na condição
