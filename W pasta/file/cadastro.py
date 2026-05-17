ed8fafc64abbe545f0e1ee36ed5bf1e47c817c0b
Henrick <henrick.souza@cs.brazcubas.edu.br>
2026-05-17 16:16:59 -0300

    Update cadastro.py

--- W pasta/file/cadastro.py
+++ W pasta/file/cadastro.py
@@ -3,17 +3,19 @@ from utils import titulo, linha
 
 
 def catagol(): #catalogo 1 
-    print("FILME | GÊNERO | ANO | DISPONÍVEL")
+    print("\n FILME | GÊNERO | ANO | DISPONÍVEL\n")
     for i, filme in enumerate(cadastro_filmes, start=1): 
         print(f"{i} - {filme['Nome']} | {filme['Genero']} | {filme['Ano']} | {filme['Disponível']}")
     #for filme in cadastro_filmes:
         #print(f"{filme['Nome']} | {filme['Genero']} | {filme['Ano']} | {filme['Disponível']}")
 
 def cadastrar(): #cadastro de filmes 2 
-    nome = str(input("Cadastrar nome do filme: "))
-    print("Gêneros cadastraveis:")
+    print("[CADASTRO DE FILMES]\n ")
+    nome = str(input("Nome do filme: "))    
+    print("\nCadastre o gênero do filme")
     print("1 - Drama | 2 - Ação | 3 - Animação | 4 - Suspense | 5 - Policial | 6 - Comedia | 7 - Ficção")
-    genero = str(input("Escolha o gênero (1 a 7): "))
+    
+    genero = str(input("\nEscolha o gênero (1 a 7): "))
     if genero == "1":
         genero = "Drama"
     elif genero == "2":
@@ -28,8 +30,10 @@ def cadastrar(): #cadastro de filmes 2
         genero = "Comédia"
     elif genero == "7":
         genero = "Ficção"
-    ano = str(input("Cadastre o ano do filme (AAAA): "))
-    disponivel = str(input("O filme está disponível? (Sim/Não): "))
+    else:
+        print("Opção invalida")    
+    ano = str(input("\nCadastre o ano do filme (AAAA): "))
+    disponivel = str(input("\nO filme está disponível? (Sim/Não): "))
     filmes = {
         "ID": len(cadastro_filmes) + 1,
         "Nome": nome,
@@ -38,10 +42,10 @@ def cadastrar(): #cadastro de filmes 2
         "Disponível": disponivel
     }
     cadastro_filmes.append(filmes)
-    print("Filme cadastrado!!!")
+    print("\nFilme cadastrado!!!")
     
-def pgenero(): #Procurar pelo gênero 8 funcionalidade bonus
-    print("Escolha por gênero")
+def pgenero(): #Procurar pelo gênero 8
+    print("\nEscolha por gênero\n")
     linha()
     print('''
         1 - Drama 
