ca3967712ae33d101648711916a7267141df8918
Henrick <henrick.souza@cs.brazcubas.edu.br>
2026-05-17 14:40:06 -0300

    Update utils.py

--- W pasta/file/utils.py
+++ W pasta/file/utils.py
@@ -1,14 +1,6 @@
-filmes
-# Um Sonho de Liberdade — Drama
-# O Poderoso Chefão — Policial / Drama
-# Batman: O Cavaleiro das Trevas — Ação / Policial
-# A Lista de Schindler — Biografia / Drama
-# Avatar — Ficção Científica / Aventura
-# Vingadores: Ultimato — Ação / Ficção Científica
-# Titanic — Romance / DramaLimite — Drama
-# Deus e o Diabo na Terra do Sol — Drama / Faroeste
-# Central do Brasil — Drama
-# Cidade de Deus — Policial / Drama
-# Zootopia 2 — Animação / Comédia
-# Uma Batalha Após a Outra — Thriller / Drama
-# Parasita — Suspense / Drama
+def linha():
+    print("-=-" * 20)
+def titulo(texto):
+    linha()
+    print(texto.center(60))
+    linha()
